75af54536c48b829bbd60cea35bd3bdad488ebc7
ariCoder <otajonovmuhammadali668@gmail.com>
2026-09-11 16:34:55 +0500

    Daily working Problem soloving

--- ChallengeProblemSolution/String/GroupAnagaramss.cs
+++ ChallengeProblemSolution/String/GroupAnagaramss.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChallengeProblemSolution.String
+﻿namespace ChallengeProblemSolution.String;
+public class GroupAnagaramss
 {
-    internal class GroupAnagaramss
+    public List<List<string>> GroupAnagrams(string[] str)
     {
+        var res = new List<List<string>>();
+
+        foreach (var item in str)
+        {
+            var groupList = GroupStringList(str);
+
+            foreach (var group in groupList)
+                res.Add(group.Value);
+            
+            return res;
+        }
+
+        return new List<List<string>>();
+    }
+
+    private static Dictionary<string, List<string>> GroupStringList(string[] strings)
+    {
+        var groupList = new Dictionary<string, List<string>>();
+
+        foreach(var item in strings)
+        {
+            var key = string.Concat(item.OrderBy(c => c));
+            if (!groupList.ContainsKey(key))
+                groupList[key] = new List<string>();
+            groupList[key].Add(item);
+        }
+
+        return groupList;
     }
 }
